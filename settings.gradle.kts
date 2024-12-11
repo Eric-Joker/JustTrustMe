c6653ca88fbec8043645165f3a1c9203db96bb44
Eric-Joker <60310368+Eric-Joker@users.noreply.github.com>
2024-12-11 12:39:14 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        maven("https://jitpack.io")
+        maven("https://api.xposed.info")
+        maven { url = uri("https://mirrors.tuna.tsinghua.edu.cn/maven/") }
+        maven{ url = uri("https://maven.aliyun.com/repository/google") }
+        //maven{ url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven{ url = uri("https://maven.aliyun.com/repository/public") }
+        maven{ url = uri("https://maven.aliyun.com/repository/jcenter") }
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://jitpack.io")
+        maven("https://api.xposed.info")
+        maven { url = uri("https://mirrors.tuna.tsinghua.edu.cn/maven/") }
+        maven{ url = uri("https://maven.aliyun.com/repository/google") }
+        //maven{ url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven{ url = uri("https://maven.aliyun.com/repository/public") }
+        maven{ url = uri("https://maven.aliyun.com/repository/jcenter") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JustTrustMe"
+include(":app")
